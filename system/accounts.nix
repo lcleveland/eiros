ac1fb46f15648cd67f69b3d2b0696eddba2535b7
Lyle Cleveland <lyle.cleveland@proton.me>
2025-11-19 11:05:46 -0600

    Add support for configurable user account creation in Eiros system

---
+++ system/accounts.nix
@@ -0,0 +1,11 @@
+{ config, lib, ... }:
+let
+  eiros_accounts = config.eiros.system.accounts;
+in
+{
+  options.eiros.system.accounts.mutable_users.enable = lib.mkEnableOption {
+    default = false;
+    description = "Allow users to create new accounts";
+  };
+  config.users.mutableUsers = eiros_accounts.mutable_users.enable;
+}
