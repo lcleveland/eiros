c3641501839438ab6efd59ffc9bbf372894be206
Lyle Cleveland <lyle.cleveland@proton.me>
2025-11-18 18:13:34 -0600

    Add support for customizable keyboard layout and variants

---
+++ system/hardware/keyboard.nix
@@ -0,0 +1,22 @@
+{ config, lib, ... }:
+let
+  eiros_hardware = config.eiros.system.hardware.keyboard;
+in
+{
+  options.eiros.system.hardware.keyboard = {
+    layout = lib.mkOption {
+      type = lib.types.str;
+      default = "us";
+      description = "Keyboard layout to use (xkb-data-style)";
+    };
+    variant = lib.mkOption {
+      type = lib.types.str;
+      default = "";
+      description = "The layout variant to use";
+    };
+  };
+  config.services.xserver.xkb = {
+    layout = eiros_hardware.keyboard.layout;
+    variant = eiros_hardware.keyboard.variant;
+  };
+}
