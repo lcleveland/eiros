fbfefe7b98019339dd47add34f60d0a144395fbf
Lyle Cleveland <lyle.cleveland@proton.me>
2025-11-19 20:07:48 -0600

    Disable adding users

--- system/accounts.nix
+++ system/accounts.nix
@@ -4,7 +4,7 @@ let
 in
 {
   options.eiros.system.accounts.mutable_users.enable = lib.mkEnableOption {
-    default = true;
+    default = false;
     description = "Allow users to create new accounts";
   };
   config.users.mutableUsers = eiros_accounts.mutable_users.enable;
