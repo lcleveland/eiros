c2c7ff18abf2169869c4c47549e9593be2cc7811
Lyle Cleveland <lyle.cleveland@proton.me>
2025-11-19 19:57:02 -0600

    Update flake to add NixOS configuration modules and inputs

--- flake.nix
+++ flake.nix
@@ -1,3 +1,27 @@
 {
   description = "Eiros: A NixOS configuration using Niri";
+  outputs =
+    { nixpkgs, self, ... }@system_inputs:
+    let
+      import_modules = import ./resources/nix/import_modules.nix;
+      inputs = system_inputs;
+      base_modules = [
+        /etc/nixos/hardware-configuration.nix
+      ]
+      ++ (import_modules ./system);
+      make_vm = nixpkgs.lib.nixosSystem {
+        modules = base_modules;
+        specialArgs = { inherit inputs; };
+      };
+    in
+    {
+      nixosConfigurations = {
+        vm = make_vm;
+      };
+    };
+  inputs = {
+    nixpkgs = {
+      url = "github:nixos/nixpkgs?ref=25.05";
+    };
+  };
 }
