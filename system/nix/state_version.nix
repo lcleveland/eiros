d787ee71d09bafce28df5bde369f83d112c5d96f
Lyle Cleveland <lyle.cleveland@proton.me>
2025-11-29 17:39:34 -0600

    Update nixpkgs version and state_version to 25.11

--- system/nix/state_version.nix
+++ system/nix/state_version.nix
@@ -4,7 +4,7 @@ let
 in
 {
   options.eiros.system.nix.state_version = lib.mkOption {
-    default = "25.05";
+    default = "25.11";
     description = "Version of the NixOS state to use.";
     type = lib.types.str;
   };
