cdbdee75def662a3042affa6913c0116c93d8ad5
Lyle Cleveland <lyle.cleveland@proton.me>
2025-12-02 17:18:52 -0600

    Modify user defaults configuration: improve extensibility and simplify module

--- users/set_user_defaults.nix
+++ users/set_user_defaults.nix
@@ -5,7 +5,7 @@
   ...
 }:
 let
-  users = builtins.attrNames options.users.users;
+  users = config.eiros.users.default_user_list;
 in
 {
   options.eiros.users.default_user_list = lib.mkOption {
@@ -13,20 +13,23 @@ in
     default = [ ];
     description = "List of users that should have default options applied.";
   };
-  config = {
-    users.users = lib.mapAttrs (name: default_config: {
-      description = lib.mkDefault name;
-      extraGroups = lib.mkDefault [
-        "wheel"
-        "networkmanager"
-      ];
-      intialPassword = lib.mkDefault name;
-      isNormalUser = lib.mkDefault true;
-      home = lib.mkDefault "/home/${name}";
-    }) users;
-    hjem.users = lib.mapAttrs (name: default_config: {
-      directory = lib.mkDefault "home/${name}";
-      user = lib.mkDefault name;
-    }) users;
-  };
+  config = lib.mkMerge (
+    map (name: {
+      users.users.${name} = {
+        description = lib.mkDefault name;
+        extraGroups = lib.mkDefault [
+          "wheel"
+          "networkmanager"
+        ];
+        initialPassword = lib.mkDefault name;
+        isNormalUser = lib.mkDefault true;
+        home = lib.mkDefault "/home/${name}";
+      };
+
+      hjem.users.${name} = {
+        directory = lib.mkDefault "/home/${name}";
+        user = lib.mkDefault name;
+      };
+    }) users
+  );
 }
