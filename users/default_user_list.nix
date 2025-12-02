fddd40fbf1706e14cf769f85c7667cc019b466ea
Lyle Cleveland <lyle.cleveland@proton.me>
2025-12-02 17:26:40 -0600

    Refine default user list module for improved clarity

--- users/default_user_list.nix
+++ users/default_user_list.nix
@@ -1,8 +1,8 @@
 { config, lib, ... }:
 {
   options.eiros.users.default_user_list = lib.mkOption {
-    type = lib.types.listOf lib.types.str;
     default = [ ];
     description = "List of users that should have default options applied.";
+    type = lib.types.listOf lib.types.str;
   };
 }
