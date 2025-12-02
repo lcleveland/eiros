9c9116c7d3b882ef934de9d6aa200af15d500ed9
Lyle Cleveland <lyle.cleveland@proton.me>
2025-12-02 17:07:30 -0600

    Refactor user defaults configuration and modularize for improved extensibility in 'set_user_defaults.nix'

--- user/set_user_defaults.nix
+++ user/set_user_defaults.nix
@@ -1,27 +1,31 @@
 {
   config,
-  lib,
   options,
+  lib,
   ...
 }:
+
 let
-  users = builtins.attrNames options.users.users;
+  userNames = builtins.attrNames options.users.users;
 in
 {
-  config = {
-    users.users = lib.mapAttrs (name: default_config: {
-      description = lib.mkDefault name;
-      extraGroups = lib.mkDefault [
-        "wheel"
-        "networkmanager"
-      ];
-      intialPassword = lib.mkDefault name;
-      isNormalUser = lib.mkDefault true;
-      home = lib.mkDefault "/home/${name}";
-    }) users;
-    hjem.users = lib.mapAttrs (name: default_config: {
-      directory = lib.mkDefault "home/${name}";
-      user = lib.mkDefault name;
-    }) users;
-  };
+  config = lib.mkMerge (
+    map (name: {
+      users.users.${name} = {
+        description = lib.mkDefault name;
+        extraGroups = lib.mkDefault [
+          "wheel"
+          "networkmanager"
+        ];
+        initialPassword = lib.mkDefault name;
+        isNormalUser = lib.mkDefault true;
+        home = lib.mkDefault "/home/${name}";
+      };
+
+      hjem.users.${name} = {
+        directory = lib.mkDefault "/home/${name}";
+        user = lib.mkDefault name;
+      };
+    }) userNames
+  );
 }
