48cf2b1f7e78622f1bbc80eeffe57d842a7ff6e9
Lyle Cleveland <lyle.cleveland@proton.me>
2025-12-03 10:33:33 -0600

    Test

--- users/users.nix
+++ users/users.nix
@@ -1,114 +1,34 @@
-{
-  config,
-  lib,
-  pkgs,
-  inputs,
-  ...
-}:
-
+{ config, lib, ... }:
 {
   options.eiros.users = lib.mkOption {
-    description = "Eiros-managed users that expand into users.users + hjem.users.";
-    type = lib.types.attrsOf (
+    default = [ ];
+    type = lib.types.listOf (
       lib.types.submodule (
-        { name, config, ... }:
+        { username, ... }:
         {
-          #### Per-Eiros-user options
-          options = {
-            enable = lib.mkOption {
-              type = lib.types.bool;
-              default = true;
-              description = "Whether to create this user.";
-            };
-
-            description = lib.mkOption {
-              type = lib.types.str;
-              default = name;
-              description = "Display name / GECOS field.";
-            };
-
-            extraGroups = lib.mkOption {
-              type = lib.types.listOf lib.types.str;
-              default = [
-                "wheel"
-                "networkmanager"
-              ];
-              description = "Extra groups for this user.";
-            };
-
-            home = lib.mkOption {
-              type = lib.types.nullOr lib.types.str;
-              default = null;
-              description = "Home directory (defaults to /home/${name}).";
-            };
-
-            shell = lib.mkOption {
-              type = lib.types.nullOr lib.types.package;
-              default = null;
-              description = "Shell (defaults to pkgs.bashInteractive).";
-            };
-
-            initialPassword = lib.mkOption {
-              type = lib.types.nullOr lib.types.str;
-              default = null;
-              description = "Initial password (defaults to username).";
-            };
-
-            hjemDirectory = lib.mkOption {
-              type = lib.types.nullOr lib.types.str;
-              default = null;
-              description = "hjem directory (defaults to same as home).";
-            };
-
-            # Extra fields merged into users.users.<name>
-            users = lib.mkOption {
-              type = lib.types.attrs;
-              default = { };
-              description = "Extra options merged into users.users.<name>.";
-            };
-
-            # Extra fields merged into hjem.users.<name>
-            hjem = lib.mkOption {
-              type = lib.types.attrs;
-              default = { };
-              description = "Extra options merged into hjem.users.<name>.";
-            };
+          extraGroups = lib.mkOption {
+            default = [
+              "wheel"
+              "networkmanager"
+            ];
+            description = "Default groups";
+            type = lib.types.listOf lib.types.str;
           };
-
-          #### Expansion into users.users + hjem.users
-          config = lib.mkIf config.enable (
-            let
-              homeDir = if config.home != null then config.home else "/home/${name}";
-
-              hjemDir = if config.hjemDirectory != null then config.hjemDirectory else homeDir;
-
-              shellPkg = if config.shell != null then config.shell else pkgs.bashInteractive;
-
-              initialPw = if config.initialPassword != null then config.initialPassword else name;
-            in
-            {
-              # NixOS user
-              users.users.${name} = {
-                isNormalUser = true;
-                description = config.description;
-                home = homeDir;
-                shell = shellPkg;
-                initialPassword = initialPw;
-                extraGroups = config.extraGroups;
-              }
-              // config.users;
-
-              # Hjem user
-              hjem.users.${name} = {
-                enable = true;
-                directory = hjemDir;
-              }
-              // config.hjem;
-            }
-          );
+          username = lib.mkOption {
+            description = "Username";
+            type = lib.types.str;
+          };
         }
       )
     );
-    default = { };
+  };
+  config = {
+    users.users = lib.listToAttrs (
+      map (user: {
+        extraGroups = lib.mkDefault user.extraGroups;
+        isNormalUser = true;
+        name = user.username;
+      }) config.eiros.users
+    );
   };
 }
