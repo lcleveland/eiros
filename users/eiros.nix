6df80197bb4bf289af113c14b0227d9bec5c235f
Lyle Cleveland <lyle.cleveland@proton.me>
2025-12-03 10:36:21 -0600

    Test

--- users/eiros.nix
+++ users/eiros.nix
@@ -14,10 +14,13 @@ in
 
   # If enabled, define eiros.users.eiros (data only, no reading from eiros.users here)
   config = lib.mkIf cfg.enable {
-    eiros.users.eiros = {
-      # You can add per-user settings here later if you like
-      # description = "Eiros";
-      # extraGroups = [ "docker" ];
-    };
+    eiros.users = lib.mkAfter [
+      {
+        username = "eiros";
+        # You can add per-user settings here later if you like
+        # description = "Eiros";
+        # extraGroups = [ "docker" ];
+      }
+    ];
   };
 }
