8c5d10584d06843f757732addd685a1b9a48d77f
Lyle Cleveland <lyle.cleveland@proton.me>
2025-12-02 18:23:25 -0600

    Revamp flake.nix to simplify configuration and optimize inputs

--- flake.nix
+++ flake.nix
@@ -1,23 +1,43 @@
-# flake.nix
 {
-  description = "Eiros minimal test";
-
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
-
+  description = "Eiros: A NixOS configuration using MangoWC";
   outputs =
-    { self, nixpkgs, ... }:
+    {
+      hjem,
+      mango,
+      nixpkgs,
+      self,
+      ...
+    }@system_inputs:
     let
-      system = "x86_64-linux";
+      import_modules = import ./resources/nix/import_modules.nix;
+      inputs = system_inputs;
+      base_modules = [
+        inputs.hjem.nixosModules.default
+        inputs.mango.nixosModules.mango
+        /etc/nixos/hardware-configuration.nix
+      ]
+      ++ (import_modules ./system)
+      ++ (import_modules ./users);
+      make_default = nixpkgs.lib.nixosSystem {
+        modules = base_modules;
+        specialArgs = { inherit inputs; };
+      };
     in
     {
-      nixosConfigurations.default = nixpkgs.lib.nixosSystem {
-        inherit system;
-        modules = [
-          ./users/default_user_list.nix
-          ./users/eiros.nix
-          ./users/set_user_defaults.nix
-          ({ ... }: { }) # empty host
-        ];
+      nixosConfigurations = {
+        default = make_default;
       };
     };
+  inputs = {
+    hjem = {
+      url = "github:feel-co/hjem";
+    };
+    mango = {
+      url = "github:DreamMaoMao/mango";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    nixpkgs = {
+      url = "github:nixos/nixpkgs?ref=25.11-beta";
+    };
+  };
 }
