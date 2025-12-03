90bdd1af8a959fb7a30953a588c7d13203e5c17d
Lyle Cleveland <lyle.cleveland@proton.me>
2025-12-02 18:55:09 -0600

    Refactor Eiros user configuration - change default user name

--- users/eiros.nix
+++ users/eiros.nix
@@ -4,14 +4,14 @@
   ...
 }:
 {
-  options.eiros.users.eiros = {
+  options.eiros.default_user = {
     enable = lib.mkOption {
       description = "Enable the default Eiros user";
       default = true;
       type = lib.types.bool;
     };
   };
-  config = lib.mkIf eiros_user.enable {
+  config = lib.mkIf config.eiros.default_user.enable {
     eiros.users.eiros = {
 
     };
