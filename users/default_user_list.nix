8267682c5cc2ec14a2dce90a21b5d460d79cfb36
Lyle Cleveland <lyle.cleveland@proton.me>
2025-12-02 18:30:35 -0600

    Refactor user management configuration and support custom extensions

--- users/default_user_list.nix
+++
@@ -1,8 +0,0 @@
-{ lib, ... }:
-{
-  options.eiros.users.default_user_list = lib.mkOption {
-    default = [ ];
-    description = "List of users that should have default options applied.";
-    type = lib.types.listOf lib.types.str;
-  };
-}
---
+++ users/users.nix
@@ -0,0 +1,93 @@
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+
+let
+  eiros_users = config.eiros.users or { };
+in
+{
+  options.eiros.users = lib.mkOption {
+    description = "Eiros-managed users";
+    type = lib.types.attrsOf (
+      lib.types.submodule (
+        { name, ... }:
+        {
+          options = {
+            enable = lib.mkOption {
+              type = lib.types.bool;
+              default = true;
+              description = "Whether to create this user.";
+            };
+
+            description = lib.mkOption {
+              type = lib.types.str;
+              default = name;
+              description = "Display name.";
+            };
+
+            extraGroups = lib.mkOption {
+              type = lib.types.listOf lib.types.str;
+              default = [
+                "wheel"
+                "networkmanager"
+              ];
+              description = "Extra groups on top of defaults.";
+            };
+
+            home = lib.mkOption {
+              type = lib.types.nullOr lib.types.str;
+              default = null;
+              description = "Home dir (defaults to ${defaultHomePrefix}/${name}).";
+            };
+
+            shell = lib.mkOption {
+              type = lib.types.nullOr lib.types.package;
+              default = null;
+              description = "Shell (defaults to eiros.defaults.shell).";
+            };
+
+            initialPassword = lib.mkOption {
+              type = lib.types.nullOr lib.types.str;
+              default = null;
+              description = "Initial password (defaults to username).";
+            };
+          };
+        }
+      )
+    );
+    default = { };
+  };
+
+  config = lib.mkMerge (
+    lib.mapAttrsToList (
+      name: ucfg:
+      lib.mkIf ucfg.enable (
+        let
+          homeDir = if ucfg.home != null then ucfg.home else "${defaultHomePrefix}/${name}";
+
+          shellPkg = if ucfg.shell != null then ucfg.shell else defaultShell;
+
+          initialPw = if ucfg.initialPassword != null then ucfg.initialPassword else name;
+        in
+        {
+          users.users.${name} = {
+            isNormalUser = true;
+            description = ucfg.description;
+            home = homeDir;
+            shell = shellPkg;
+            initialPassword = initialPw;
+            extraGroups = defaultGroups ++ ucfg.extraGroups;
+          };
+
+          hjem.users.${name} = {
+            user = name;
+            directory = homeDir;
+          };
+        }
+      )
+    ) eiros_users
+  );
+}
