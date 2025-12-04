5bb980a5dccf8731244814baae5bf10b8a8d06ac
Lyle Cleveland <lyle.cleveland@proton.me>
2025-12-03 18:53:18 -0600

    Refactor user configuration: simplify and improve naming consistency

--- users/default_user.nix
+++ users/default_user.nix
@@ -12,12 +12,8 @@ in
     };
   };
 
-  # If enabled, define eiros.users.eiros (data only, no reading from eiros.users here)
   config = lib.mkIf cfg.enable {
     eiros.users.eiros = {
-      # You can add per-user settings here later if you like
-      # description = "Eiros";
-      # extraGroups = [ "docker" ];
     };
   };
 }
