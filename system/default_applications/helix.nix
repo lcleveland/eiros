a9a462a322b24d3dd0f08e5e57058f0630fd2314
Lyle Cleveland <lyle.cleveland@proton.me>
2025-12-05 14:00:53 -0600

    Add helix (https://helix-editor.com/) as a new default application with configurable enablement

---
+++ system/default_applications/helix.nix
@@ -0,0 +1,17 @@
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+let
+  eiros_helix = config.eiros.system.default_applications.helix;
+in
+{
+  options.eiros.system.default_applications.helix.enable = {
+    default = true;
+    description = "Enables helix (https://helix-editor.com/)";
+    type = lib.types.bool;
+  };
+  config.environment.systemPackages = lib.mkIf eiros_helix.enable [ pkgs.helix ];
+}
