377f13d02882447c2466b6f0f7ed3d1dee48c114
Lyle Cleveland <lyle.cleveland@proton.me>
2025-12-07 16:16:56 -0600

    Update window compositor in dank material shell for improved compatibility

--- system/desktop_environment/dank_material_shell.nix
+++ system/desktop_environment/dank_material_shell.nix
@@ -19,7 +19,7 @@ in
     enable = true;
     greeter = lib.mkIf eiros_dms.greeter.enable {
       enable = true;
-      compositor.name = "sway";
+      compositor.name = "hyprland";
     };
     systemd = {
       enable = true;
