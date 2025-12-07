325e26448a00f470728d14fe9fe3dff46bccafac
Lyle Cleveland <lyle.cleveland@proton.me>
2025-12-07 10:19:38 -0600

    Add firmware options and enable all supported firmware configuration

---
+++ system/hardware/firmware.nix
@@ -0,0 +1,14 @@
+{ config, lib, ... }:
+let
+  eiros_firmware = config.eiros.system.hardware.firmware;
+in
+{
+  options.eiros.system.hardware.firmware = {
+    enable_all_firmware = lib.mkOption {
+      default = true;
+      description = "Enable all supported firmware";
+      types = lib.types.bool;
+    };
+  };
+  config.hardware.enableAllFirmware = eiros_firmware.enable_all_firmware;
+}
