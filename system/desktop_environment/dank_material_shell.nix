8af4226af555ef502fb7142567634b58bada26c5
Lyle Cleveland <lyle.cleveland@proton.me>
2025-12-06 22:43:20 -0600

    Change compositor from "mangowc" to "niri" in Dank Material Shell

--- system/desktop_environment/dank_material_shell.nix
+++ system/desktop_environment/dank_material_shell.nix
@@ -19,7 +19,7 @@ in
     enable = true;
     greeter = lib.mkIf eiros_dms.greeter.enable {
       enable = true;
-      compositor.name = "mangowc";
+      compositor.name = "niri";
     };
     systemd = {
       enable = true;
