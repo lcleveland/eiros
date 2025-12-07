985986764ccac658b67c31ead179a758c23aac28
Lyle Cleveland <lyle.cleveland@proton.me>
2025-12-07 10:32:58 -0600

    Implement Pipewire as audio backend and optional configuration

---
+++ system/pipewire.nix
@@ -0,0 +1,23 @@
+{ config, lib, ... }:
+let
+  eiros_pipewire = config.eiros.system.pipewire;
+in
+{
+  options.eiros.system.pipewire = {
+    enable = lib.mkOption {
+      default = true;
+      description = "Enable pipewire";
+      type = lib.types.bool;
+    };
+  };
+  config = lib.mkIf eiros_pipewire.enable {
+    hardware.pulseaudio.enable = false;
+    services.pipewire = {
+      enable = true;
+      alsa.enable = true;
+      alsa.support32Bit = true;
+      pulse.enable = true;
+    };
+    security.rtkit.enable = true;
+  };
+}
