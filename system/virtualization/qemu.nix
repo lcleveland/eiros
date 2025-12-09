0d69229d1fd6441276150116ead2c83357cf37a0
Lyle Cleveland <lyle.cleveland@proton.me>
2025-12-09 09:45:10 -0600

    Add qemu virtualization support with configuration options

---
+++ system/virtualization/qemu.nix
@@ -0,0 +1,22 @@
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+let
+  eiros_virtualization = config.eiros.system.virtualization;
+  eiros_qemu = config.eiros.system.virtualization.qemu;
+in
+{
+  options.eiros.system.virtualization.qemu = {
+    enable = lib.mkOption {
+      default = true;
+      description = "Enable qemu virtual machines";
+      types = lib.types.bool;
+    };
+  };
+  config = lib.mkIf (eiros_virtualization.enable && eiros_qemu.enable) {
+    environment.systemPackages = [ pkgs.qemu ];
+  };
+}
