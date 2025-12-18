c216f7d6aa4fd7faa6c3706f70d0ced48a2ff47b
Lyle Cleveland <lyle.cleveland@proton.me>
2025-12-18 14:06:56 -0600

    Remove Helix, add support for NeoVim as default editor

---
+++ system/default_applications/neovim.nix
@@ -0,0 +1,12 @@
+{ config, lib, ... }:
+let
+  eiros_neovim = config.eiros.system.default_applications.neovim;
+in
+{
+  options.eiros.system.default_applications.neovim.enable = lib.mkOption {
+    default = true;
+    description = "Whether or not to use NeoVim as the editor for Eiros";
+    type = lib.types.bool;
+  };
+  config.programs.neovim.defaultEditor = eiros_neovim.enable;
+}
