778d2b2414a9798c53b63976ba9edf90d2d74feb
Lyle Cleveland <lyle.cleveland@proton.me>
2025-12-21 13:48:28 -0600

    Update flake.nix for user and hardware configuration

--- flake.nix
+++ flake.nix
@@ -18,13 +18,13 @@
       base_modules = [
         inputs.dank_material_shell.nixosModules.dank-material-shell
         inputs.dank_material_shell.nixosModules.greeter
+        inputs.eiros_hardware.nixosModules.default
         inputs.hjem.nixosModules.default
         inputs.mango.nixosModules.mango
       ]
       ++ (import_modules ./system)
       ++ (import_modules ./users)
-      ++ eiros_users.nixosModules.default
-      ++ eiros_hardware.nixosModules.default;
+      ++ eiros_users.nixosModules.default;
       make_default = nixpkgs.lib.nixosSystem {
         modules = base_modules;
         specialArgs = { inherit inputs; };
