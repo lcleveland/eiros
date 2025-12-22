9bbb48b5e4eeb0e6046eaa2477dffe3bb49c0412
Lyle Cleveland <lyle.cleveland@proton.me>
2025-12-22 09:37:28 -0600

    Update Dank Material Shell with customizable greeter and Hyprland config sections

--- system/desktop_environment/dank_material_shell.nix
+++ system/desktop_environment/dank_material_shell.nix
@@ -1,18 +1,108 @@
 { config, lib, ... }:
 let
   eiros_dms = config.eiros.system.desktop_environment.dank_material_shell;
+  hypr_value_type = lib.types.either lib.types.str (lib.types.listOf lib.types.str);
+  to_non_empty_list =
+    v:
+    let
+      lst = if lib.isList v then v else [ v ];
+    in
+    lib.filter (s: s != "") (map toString lst);
+  render_hypr_config =
+    sections:
+    let
+      render_kv_lines =
+        kv:
+        let
+          pairs = lib.concatLists (
+            lib.mapAttrsToList (
+              k: v:
+              let
+                vals = to_non_empty_list v;
+              in
+              map (vv: "${k} = ${vv}") vals
+            ) kv
+          );
+        in
+        lib.concatStringsSep "\n" pairs;
+      indent_lines = s: lib.concatStringsSep "\n" (map (l: "  " + l) (lib.splitString "\n" s));
+      render_section =
+        section_name: kv:
+        let
+          body = render_kv_lines kv;
+        in
+        if body == "" then
+          ""
+        else if section_name == "" || section_name == null then
+          body
+        else
+          ''
+            ${section_name} {
+            ${indent_lines body}
+            }
+          '';
+      rendered = lib.concatStringsSep "\n\n" (
+        lib.filter (s: s != "") (lib.mapAttrsToList render_section sections)
+      );
+    in
+    rendered;
 in
 {
   options.eiros.system.desktop_environment.dank_material_shell = {
     enable = lib.mkOption {
+      type = lib.types.bool;
       default = true;
       description = "Enable the Eiros Dank Material Shell";
-      type = lib.types.bool;
     };
-    greeter.enable = lib.mkOption {
-      default = true;
-      description = "Enable the Eiros Dank Material Shell Greeter";
-      type = lib.types.bool;
+    greeter = {
+      enable = lib.mkOption {
+        type = lib.types.bool;
+        default = true;
+        description = "Enable the Eiros Dank Material Shell Greeter";
+      };
+      hyprland = {
+        sections = lib.mkOption {
+          type = lib.types.attrsOf (lib.types.attrsOf hypr_value_type);
+          default = { };
+          description = ''
+            Hyprland config sections for the greeter.
+
+            Each section is an attrset of keys to either:
+              - a string (single line)
+              - a list of strings (repeated key lines)
+
+            Special section name "":
+              - renders top-level lines (no section wrapper)
+          '';
+          example = {
+            input = {
+              kb_layout = "us";
+              kb_variant = "dvorak";
+              kb_options = [
+                "caps:escape"
+                "compose:ralt"
+              ];
+            };
+
+            "" = {
+              monitor = [
+                ",preferred,auto,1"
+                "HDMI-A-1,preferred,auto,1"
+              ];
+              exec-once = [
+                "dbus-update-activation-environment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP"
+                "dunst"
+              ];
+            };
+            "" = {
+              bind = [
+                "SUPER,Return,exec,kitty"
+                "SUPER,Q,killactive,"
+              ];
+            };
+          };
+        };
+      };
     };
   };
   config.programs.dank-material-shell = lib.mkIf eiros_dms.enable {
@@ -20,6 +110,7 @@ in
     greeter = lib.mkIf eiros_dms.greeter.enable {
       enable = true;
       compositor.name = "hyprland";
+      customConfig = render_hypr_config eiros_dms.greeter.hyprland.sections;
     };
     systemd = {
       enable = true;
