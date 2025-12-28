f5fd041bd9e48f0ab00cd463ee2c380f2263653e
Lyle Cleveland <lyle.cleveland@proton.me>
2025-12-28 14:30:20 -0600

    fix: correct module name casing for dankMaterialShell in flake.nix

--- flake.nix
+++ flake.nix
@@ -15,7 +15,7 @@
       import_modules = import ./resources/nix/import_modules.nix;
       inputs = system_inputs;
       base_modules = [
-        inputs.dank_material_shell.nixosModules.dank-material-shell
+        inputs.dank_material_shell.nixosModules.dankMaterialShell
         inputs.dank_material_shell.nixosModules.greeter
         inputs.eiros_hardware.nixosModules.default
         inputs.eiros_users.nixosModules.default
