f3f114d35e180835f07c78b708209825d7a19ecc
Lyle Cleveland <lyle.cleveland@proton.me>
2025-12-30 11:49:08 -0600

    add cuda support

--- system/hardware/graphics.nix
+++ system/hardware/graphics.nix
@@ -1,27 +1,67 @@
-{ config, lib, ... }:
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
 let
   eiros_graphics = config.eiros.system.hardware.graphics;
+  eiros_nvidia = eiros_graphics.nvidia;
 in
 {
   options.eiros.system.hardware.graphics = {
     enable = lib.mkOption {
+      type = lib.types.bool;
       default = true;
       description = "Enable Eiros hardware graphics.";
-      type = lib.types.bool;
     };
-    nvidia.enable = lib.mkOption {
-      default = true;
-      description = "Enable Nvidia GPU support.";
-      type = lib.types.bool;
+
+    nvidia = {
+      enable = lib.mkOption {
+        type = lib.types.bool;
+        default = true;
+        description = "Enable Nvidia GPU support.";
+      };
+
+      cuda = {
+        enable = lib.mkOption {
+          type = lib.types.bool;
+          default = false;
+          description = "Enable CUDA support (toolkit + nixpkgs cudaSupport).";
+        };
+
+        add_cache = lib.mkOption {
+          type = lib.types.bool;
+          default = true;
+          description = "Add nixos-cuda binary cache via extra-substituters.";
+        };
+      };
     };
   };
+
   config = lib.mkIf eiros_graphics.enable {
     hardware.graphics.enable = true;
-    services.xserver.videoDrivers = lib.mkIf eiros_graphics.nvidia.enable [ "nvidia" ];
-    hardware.nvidia = lib.mkIf eiros_graphics.nvidia.enable {
-    modesetting.enable = true;
-    open = true;
-    nvidiaSettings = true;
+    services.xserver.videoDrivers = lib.mkIf eiros_nvidia.enable [ "nvidia" ];
+    hardware.nvidia = lib.mkIf eiros_nvidia.enable {
+      modesetting.enable = true;
+      open = true;
+      nvidiaSettings = true;
     };
+    nixpkgs.config.cudaSupport = lib.mkIf (eiros_nvidia.enable && eiros_nvidia.cuda.enable) true;
+    environment.systemPackages = lib.mkIf (eiros_nvidia.enable && eiros_nvidia.cuda.enable) (
+      with pkgs;
+      [
+        cudaPackages.cudatoolkit
+        nvtopPackages.nvidia
+      ]
+    );
+    nix.settings =
+      lib.mkIf (eiros_nvidia.enable && eiros_nvidia.cuda.enable && eiros_nvidia.cuda.add_cache)
+        {
+          extra-substituters = [ "https://cache.nixos-cuda.org" ];
+          extra-trusted-public-keys = [
+            "cache.nixos-cuda.org:74DUi4Ye579gUqzH4ziL9IyiJBlDpMRn9MBN8oNan9M="
+          ];
+        };
   };
 }
