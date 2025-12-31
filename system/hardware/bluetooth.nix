5e8d0642dab94f44fd93592186058aa7226c96a7
Lyle Cleveland <lyle.cleveland@proton.me>
2025-12-30 20:47:24 -0600

    refactor

--- system/hardware/bluetooth.nix
+++ system/hardware/bluetooth.nix
@@ -6,9 +6,41 @@ in
   options.eiros.system.hardware.bluetooth = {
     enable = lib.mkOption {
       default = true;
-      description = "Enable bluetooth support";
+      description = "Enable Bluetooth support.";
       type = lib.types.bool;
     };
+
+    pipewire = {
+      enable = lib.mkOption {
+        default = true;
+        description = "Enable Bluetooth audio support optimized for PipeWire.";
+        type = lib.types.bool;
+      };
+
+      roles = lib.mkOption {
+        default = [
+          "Source"
+          "Sink"
+          "Media"
+          "Socket"
+        ];
+        description = "Bluetooth audio roles to enable (PipeWire/BlueZ).";
+        type = lib.types.listOf lib.types.str;
+      };
+    };
   };
-  config.hardware.bluetooth.enable = eiros_bluetooth.enable;
+
+  config = lib.mkIf eiros_bluetooth.enable {
+    hardware = {
+      bluetooth = {
+        enable = true;
+
+        settings = lib.mkIf eiros_bluetooth.pipewire.enable {
+          General = {
+            Enable = lib.concatStringsSep "," eiros_bluetooth.pipewire.roles;
+          };
+        };
+      };
+    };
+  };
 }
