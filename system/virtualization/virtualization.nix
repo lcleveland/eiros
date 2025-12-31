a5347cd45086d49d4a96ef5086b9c22ce323a046
Lyle Cleveland <lyle.cleveland@proton.me>
2025-12-31 08:42:29 -0600

    refactor

--- system/virtualization/virtualization.nix
+++ system/virtualization/virtualization.nix
@@ -3,9 +3,15 @@ let
   eiros_virtualization = config.eiros.system.virtualization;
 in
 {
-  options.eiros.system.virtualization.enable = lib.mkOption {
-    default = true;
-    description = "Enable virtualization support";
-    type = lib.types.bool;
+  options.eiros.system.virtualization = {
+    enable = lib.mkOption {
+      default = true;
+      description = "Enable virtualization support.";
+      type = lib.types.bool;
+    };
   };
+
+  config.warnings = lib.optionals (!eiros_virtualization.enable) [
+    "Virtualization is disabled; all virtualization-related modules should be inactive."
+  ];
 }
