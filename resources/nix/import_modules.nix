8de87b5475fe9aead2f5bcc64b667e88dfe52f00
Lyle Cleveland <lyle.cleveland@proton.me>
2025-12-31 09:01:49 -0600

    refactor

--- resources/nix/import_modules.nix
+++ resources/nix/import_modules.nix
@@ -1,31 +1,40 @@
 dir:
 let
   inherit (builtins)
-    readDir
     attrNames
-    filter
-    match
     concatLists
+    match
+    readDir
+    sort
     ;
-  collectImports =
-    relPath:
+
+  is_private = name: match "^[._].*" name != null;
+
+  sorted_public_names =
+    entries: sort (a: b: a < b) (builtins.filter (n: !is_private n) (attrNames entries));
+
+  collect_imports =
+    rel_path:
     let
-      fullPath = if relPath == "" then dir else dir + "/${relPath}";
-      entries = readDir fullPath;
-      processEntry =
+      full_path = if rel_path == "" then dir else dir + "/${rel_path}";
+
+      entries = readDir full_path;
+
+      process_entry =
         name:
         let
-          subRelPath = if relPath == "" then name else "${relPath}/${name}";
-          entryPath = dir + "/${subRelPath}";
-          entryType = entries.${name};
+          sub_rel_path = if rel_path == "" then name else "${rel_path}/${name}";
+
+          entry_path = dir + "/${sub_rel_path}";
+          entry_type = entries.${name};
         in
-        if entryType == "directory" then
-          collectImports subRelPath
-        else if entryType == "regular" && match ".*\\.nix" name != null then
-          [ (import entryPath) ]
+        if entry_type == "directory" then
+          collect_imports sub_rel_path
+        else if entry_type == "regular" && match ".*\\.nix" name != null then
+          [ (import entry_path) ]
         else
           [ ];
     in
-    concatLists (map processEntry (attrNames entries));
+    concatLists (map process_entry (sorted_public_names entries));
 in
-collectImports ""
+collect_imports ""
