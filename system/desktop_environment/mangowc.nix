63e4edd716ab94f7cbf7721044f0fa279961b422
Lyle Cleveland <lyle.cleveland@proton.me>
2025-12-30 20:34:55 -0600

    refactor

--- system/desktop_environment/mangowc.nix
+++ system/desktop_environment/mangowc.nix
@@ -8,13 +8,26 @@ let
   eiros_mangowc = config.eiros.system.desktop_environment.mangowc;
 in
 {
-  options.eiros.system.desktop_environment.mangowc.enable = lib.mkOption {
-    description = "Enables the Mango Window Composer.";
-    default = true;
-    type = lib.types.bool;
+  options.eiros.system.desktop_environment.mangowc = {
+    enable = lib.mkOption {
+      default = true;
+      description = "Enable the Mango Window Composer.";
+      type = lib.types.bool;
+    };
+
+    package = lib.mkOption {
+      default = pkgs.mango;
+      description = "Mango Window Composer package to use.";
+      type = lib.types.package;
+    };
   };
 
   config = lib.mkIf eiros_mangowc.enable {
-    programs.mango.enable = true;
+    programs = {
+      mango = {
+        enable = true;
+        package = eiros_mangowc.package;
+      };
+    };
   };
 }
