edbfa0830221a746b9a33ef2c87fe61af6080874
Lyle Cleveland <lyle.cleveland@proton.me>
2025-12-30 20:57:03 -0600

    refactor

--- system/hardware/firmware.nix
+++ system/hardware/firmware.nix
@@ -1,4 +1,9 @@
-{ config, lib, ... }:
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
 let
   eiros_firmware = config.eiros.system.hardware.firmware;
 in
@@ -6,9 +11,27 @@ in
   options.eiros.system.hardware.firmware = {
     enable_all_firmware = lib.mkOption {
       default = true;
-      description = "Enable all supported firmware";
+      description = "Enable all supported firmware.";
       type = lib.types.bool;
     };
+
+    extra_packages = lib.mkOption {
+      default = [ ];
+      description = "Additional firmware packages to install (added to hardware.firmware).";
+      type = lib.types.listOf lib.types.package;
+    };
   };
-  config.hardware.enableAllFirmware = eiros_firmware.enable_all_firmware;
+
+  config = {
+    warnings =
+      lib.optionals (eiros_firmware.enable_all_firmware && !(config.nixpkgs.config.allowUnfree or false))
+        [
+          "eiros.system.hardware.firmware.enable_all_firmware is enabled, but nixpkgs.config.allowUnfree is false; some firmware may be unavailable."
+        ];
+
+    hardware = {
+      enableAllFirmware = eiros_firmware.enable_all_firmware;
+      firmware = eiros_firmware.extra_packages;
+    };
+  };
 }
