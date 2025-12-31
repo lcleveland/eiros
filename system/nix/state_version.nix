386ded23fe4f89a25a6814d7afcda76d033a7443
Lyle Cleveland <lyle.cleveland@proton.me>
2025-12-31 08:18:12 -0600

    refactor

--- system/nix/state_version.nix
+++ system/nix/state_version.nix
@@ -3,10 +3,19 @@ let
   eiros_nix = config.eiros.system.nix;
 in
 {
-  options.eiros.system.nix.state_version = lib.mkOption {
-    default = "25.11";
-    description = "Version of the NixOS state to use.";
-    type = lib.types.str;
+  options.eiros.system.nix = {
+    state_version = lib.mkOption {
+      default = "25.11";
+      description = "Version of the NixOS state to use.";
+      type = lib.types.str;
+    };
   };
-  config.system.stateVersion = eiros_nix.state_version;
+
+  config = {
+    warnings = lib.optionals (config.system.stateVersion != eiros_nix.state_version) [
+      "Changing system.stateVersion after installation can break existing systems. This should normally only be set once."
+    ];
+
+    system.stateVersion = eiros_nix.state_version;
+  };
 }
