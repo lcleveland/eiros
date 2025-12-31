113a81c862e260b869dfec2dd69bc895bb9a0484
Lyle Cleveland <lyle.cleveland@proton.me>
2025-12-30 21:08:39 -0600

    refactor

--- system/hardware/usb_storage.nix
+++ system/hardware/usb_storage.nix
@@ -5,20 +5,28 @@
   ...
 }:
 let
-  eiros_usb = config.eiros.system.hardware.usb_storage;
+  eiros_usb_storage = config.eiros.system.hardware.usb_storage;
 in
 {
   options.eiros.system.hardware.usb_storage = {
     udiskie.enable = lib.mkOption {
       default = true;
-      description = "Mount USB storage device";
+      description = "Enable USB storage automount support (udisks2 + udiskie).";
       type = lib.types.bool;
     };
   };
+
   config = {
-    services.udisks2.enable = eiros_usb.udiskie.enable;
-    environment.systemPackages = lib.mkIf (eiros_usb.udiskie.enable) [
-      pkgs.udiskie
-    ];
+    services = {
+      udisks2 = {
+        enable = eiros_usb_storage.udiskie.enable;
+      };
+    };
+
+    environment = {
+      systemPackages = lib.mkIf eiros_usb_storage.udiskie.enable [
+        pkgs.udiskie
+      ];
+    };
   };
 }
