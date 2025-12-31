57882e0d30320890df45ce6048422cf96d41f249
Lyle Cleveland <lyle.cleveland@proton.me>
2025-12-30 19:50:08 -0600

    refactor

--- system/boot/kernel/kernel_package.nix
+++ system/boot/kernel/kernel_package.nix
@@ -9,9 +9,18 @@ let
 in
 {
   options.eiros.system.boot.kernel.kernel_package = lib.mkOption {
-    type = lib.types.attrs;
     default = pkgs.linuxPackages_latest;
-    description = "The kernel package to use for Eiros.";
+    description = "The kernel package set to use for Eiros (e.g. pkgs.linuxPackages_latest).";
+    type = lib.types.attrs;
   };
+
+  assertions = [
+    {
+      assertion =
+        (eiros_kernel.kernel_package ? kernel) && (lib.isDerivation eiros_kernel.kernel_package.kernel);
+      message = "eiros.system.boot.kernel.kernel_package must be a linuxPackages set (an attrset containing a derivation at .kernel), e.g. pkgs.linuxPackages_latest.";
+    }
+  ];
+
   config.boot.kernelPackages = eiros_kernel.kernel_package;
 }
