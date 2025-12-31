4156936933a116a8cd46778e135bf171e9ddad68
Lyle Cleveland <lyle.cleveland@proton.me>
2025-12-30 20:09:12 -0600

    refactor

--- system/default_applications/git.nix
+++ system/default_applications/git.nix
@@ -3,10 +3,13 @@ let
   eiros_git = config.eiros.system.default_applications.git;
 in
 {
-  options.eiros.system.default_applications.git.enable = lib.mkOption {
-    description = "Whether to enable git";
-    default = true;
-    type = lib.types.bool;
+  options.eiros.system.default_applications.git = {
+    enable = lib.mkOption {
+      default = true;
+      description = "Enable Git.";
+      type = lib.types.bool;
+    };
   };
+
   config.programs.git.enable = eiros_git.enable;
 }
