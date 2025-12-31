9e494a2c88019542286442f4bc0b2e8bee28fa08
Lyle Cleveland <lyle.cleveland@proton.me>
2025-12-30 20:50:28 -0600

    refactor

--- system/hardware/cpu.nix
+++ system/hardware/cpu.nix
@@ -1,22 +1,60 @@
-{ config, lib, ... }:
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
 let
-  eiros_hardware = config.eiros.system.hardware.cpu;
+  eiros_cpu = config.eiros.system.hardware.cpu;
+
+  detected_vendor =
+    let
+      vendor = lib.toLower (pkgs.stdenv.hostPlatform.parsed.cpu.vendor or "");
+    in
+    if
+      lib.elem vendor [
+        "amd"
+        "intel"
+      ]
+    then
+      vendor
+    else
+      null;
+
+  effective_vendor = if eiros_cpu.vendor != null then eiros_cpu.vendor else detected_vendor;
 in
 {
   options.eiros.system.hardware.cpu = {
     microcode.enable = lib.mkOption {
       default = true;
-      description = "Enables or disables CPU microcode support";
+      description = "Enable CPU microcode updates.";
       type = lib.types.bool;
     };
+
     vendor = lib.mkOption {
-      default = "amd";
-      description = "Sets the CPU vendor";
-      type = lib.types.enum [
-        "amd"
-        "intel"
-      ];
+      default = null;
+      description = "CPU vendor. If null, auto-detect from hostPlatform.";
+      type = lib.types.nullOr (
+        lib.types.enum [
+          "amd"
+          "intel"
+        ]
+      );
     };
   };
-  config.hardware.cpu.${eiros_hardware.vendor}.updateMicrocode = eiros_hardware.microcode.enable;
+
+  config = {
+    assertions = [
+      {
+        assertion = effective_vendor != null;
+        message = "Unable to auto-detect CPU vendor; set eiros.system.hardware.cpu.vendor to \"amd\" or \"intel\".";
+      }
+    ];
+
+    hardware = {
+      cpu = {
+        ${effective_vendor}.updateMicrocode = eiros_cpu.microcode.enable;
+      };
+    };
+  };
 }
