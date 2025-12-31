d0c503e999f9c5917b579d45867278c0d5523daf
Lyle Cleveland <lyle.cleveland@proton.me>
2025-12-30 20:44:44 -0600

    refactor

--- system/fonts/fontconfig.nix
+++ system/fonts/fontconfig.nix
@@ -1,9 +1,43 @@
 { config, lib, ... }:
+let
+  eiros_font_config = config.eiros.system.fonts.font_config;
+in
 {
-  options.eiros.system.fonts.font_config.enable = lib.mkOption {
-    default = true;
-    description = "Enable fontconfig";
-    type = lib.types.bool;
+  options.eiros.system.fonts.font_config = {
+    enable = lib.mkOption {
+      default = true;
+      description = "Enable Fontconfig.";
+      type = lib.types.bool;
+    };
+
+    default_fonts = {
+      monospace = lib.mkOption {
+        default = [ "JetBrainsMono" ];
+        description = "Default monospace fonts.";
+        type = lib.types.listOf lib.types.str;
+      };
+
+      sans_serif = lib.mkOption {
+        default = [ "Inter" ];
+        description = "Default sans-serif fonts.";
+        type = lib.types.listOf lib.types.str;
+      };
+
+      serif = lib.mkOption {
+        default = [ "Noto Serif" ];
+        description = "Default serif fonts.";
+        type = lib.types.listOf lib.types.str;
+      };
+    };
   };
-  config.fonts.fontconfig.enable = config.eiros.system.fonts.font_config.enable;
+
+  config.fonts.fontconfig = {
+    enable = eiros_font_config.enable;
+
+    defaultFonts = {
+      monospace = eiros_font_config.default_fonts.monospace;
+      sansSerif = eiros_font_config.default_fonts.sans_serif;
+      serif = eiros_font_config.default_fonts.serif;
+    };
+  };
 }
