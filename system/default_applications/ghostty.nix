2d5611c9a00d849cc6cdb97388d5606607aa7915
Lyle Cleveland <lyle.cleveland@proton.me>
2025-12-30 20:07:20 -0600

    refactor

--- system/default_applications/ghostty.nix
+++ system/default_applications/ghostty.nix
@@ -8,12 +8,19 @@ let
   eiros_ghostty = config.eiros.system.default_applications.ghostty;
 in
 {
-  options.eiros.system.default_applications.ghostty.enable = lib.mkOption {
-    description = "Enable ghostty terminal";
-    default = true;
-    type = lib.types.bool;
+  options.eiros.system.default_applications.ghostty = {
+    enable = lib.mkOption {
+      default = true;
+      description = "Enable Ghostty terminal";
+      type = lib.types.bool;
+    };
+
+    package = lib.mkOption {
+      default = pkgs.ghostty;
+      description = "Ghostty package to install.";
+      type = lib.types.package;
+    };
   };
-  config.environment.systemPackages = lib.mkIf eiros_ghostty.enable [
-    pkgs.ghostty
-  ];
+
+  config.environment.systemPackages = lib.mkIf eiros_ghostty.enable [ eiros_ghostty.package ];
 }
