fbb0e4c7de71cbb1a3e034d2cc463a94b5e2991b
Lyle Cleveland <lyle.cleveland@proton.me>
2025-12-31 08:15:25 -0600

    refactor

--- system/nix/host_platform.nix
+++ system/nix/host_platform.nix
@@ -3,15 +3,18 @@ let
   eiros_nix = config.eiros.system.nix;
 in
 {
-  options.eiros.system.nix.host_platform = lib.mkOption {
-    default = "x86_64-linux";
-    description = "The platform that the nix daemon is running on.";
-    type = lib.types.enum [
-      "aarch64-darwin"
-      "aarch64-linux"
-      "x86_64-darwin"
-      "x86_64-linux"
-    ];
+  options.eiros.system.nix = {
+    host_platform = lib.mkOption {
+      default = "x86_64-linux";
+      description = "The platform that the Nix daemon is running on.";
+      type = lib.types.enum [
+        "aarch64-darwin"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "x86_64-linux"
+      ];
+    };
   };
+
   config.nixpkgs.hostPlatform = eiros_nix.host_platform;
 }
