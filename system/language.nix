415f7944ec76751eff335edec1a3bb26c0ba8726
Lyle Cleveland <lyle.cleveland@proton.me>
2025-12-30 19:44:51 -0600

    refactor

--- system/language.nix
+++ system/language.nix
@@ -4,22 +4,24 @@ let
 in
 {
   options.eiros.system.language.locale = lib.mkOption {
-    type = lib.types.str;
     default = "en_US.UTF-8";
     description = "Language locale for the system (e.g. en_US.UTF-8)";
+    type = lib.types.str;
   };
+
   config.i18n = {
     defaultLocale = eiros_language.locale;
+
     extraLocaleSettings = {
-      LC_TIME = eiros_language.locale;
-      LC_MONETARY = eiros_language.locale;
-      LC_PAPER = eiros_language.locale;
-      LC_NAME = eiros_language.locale;
       LC_ADDRESS = eiros_language.locale;
-      LC_TELEPHONE = eiros_language.locale;
-      LC_MEASUREMENT = eiros_language.locale;
       LC_IDENTIFICATION = eiros_language.locale;
+      LC_MEASUREMENT = eiros_language.locale;
+      LC_MONETARY = eiros_language.locale;
+      LC_NAME = eiros_language.locale;
       LC_NUMERIC = eiros_language.locale;
+      LC_PAPER = eiros_language.locale;
+      LC_TELEPHONE = eiros_language.locale;
+      LC_TIME = eiros_language.locale;
     };
   };
 }
