3601e073dde1c72736abafa20ad949fc8900a6ee
Lyle Cleveland <lyle.cleveland@proton.me>
2025-12-30 20:16:11 -0600

    refactor

--- system/default_applications/vivaldi.nix
+++ system/default_applications/vivaldi.nix
@@ -8,10 +8,46 @@ let
   eiros_vivaldi = config.eiros.system.default_applications.vivaldi;
 in
 {
-  options.eiros.system.default_applications.vivaldi.enable = lib.mkOption {
-    default = true;
-    description = "Enable vivaldi as the default browser.";
-    type = lib.types.bool;
+  options.eiros.system.default_applications.vivaldi = {
+    desktop_file = lib.mkOption {
+      default = "vivaldi.desktop";
+      description = "Desktop file used for default browser associations.";
+      type = lib.types.str;
+    };
+
+    enable = lib.mkOption {
+      default = true;
+      description = "Enable Vivaldi as the default browser.";
+      type = lib.types.bool;
+    };
+
+    package = lib.mkOption {
+      default = pkgs.vivaldi;
+      description = "Vivaldi package to install.";
+      type = lib.types.package;
+    };
   };
-  config.environment.systemPackages = lib.mkIf eiros_vivaldi.enable [ pkgs.vivaldi ];
+
+  config = lib.mkIf eiros_vivaldi.enable {
+    assertions = [
+      {
+        assertion = config.nixpkgs.config.allowUnfree or false;
+        message = "Vivaldi requires nixpkgs.config.allowUnfree = true.";
+      }
+    ];
+
+    environment = {
+      systemPackages = [ eiros_vivaldi.package ];
+    };
+
+    xdg = {
+      mime = {
+        defaultApplications = {
+          "text/html" = [ eiros_vivaldi.desktop_file ];
+          "x-scheme-handler/http" = [ eiros_vivaldi.desktop_file ];
+          "x-scheme-handler/https" = [ eiros_vivaldi.desktop_file ];
+        };
+      };
+    };
+  };
 }
