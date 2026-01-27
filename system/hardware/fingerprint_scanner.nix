0b18d81a4985016af99bc36cfcdf83c6b2081b0c
Lyle Cleveland <lyle.cleveland@proton.me>
2026-01-27 13:36:26 -0600

    add fprintd support

---
+++ system/hardware/fingerprint_scanner.nix
@@ -0,0 +1,19 @@
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+let
+  eiros_fingerprint_scanner = config.eiros.system.hardware.fingerprint_scanner;
+in
+{
+  options.eiros.system.hardware.fingerprint_scanner = {
+    enable = lib.mkOption {
+      default = true;
+      description = "Enable Eiros hardware fingerprint scanner support.";
+      type = lib.types.bool;
+    };
+  };
+  config.services.fprinted.enable = eiros_fingerprint_scanner.enable;
+}
