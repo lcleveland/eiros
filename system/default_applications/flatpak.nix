b9069ec47feeb875240a56e83aad70385deae317
Lyle Cleveland <lyle.cleveland@proton.me>
2026-03-05 12:51:33 -0600

    add enhanced flatpak support

--- system/default_applications/flatpak.nix
+++ system/default_applications/flatpak.nix
@@ -5,7 +5,17 @@
   ...
 }:
 let
-  eiros_flatpak = config.eiros.system.default_applications.flatpak;
+  cfg = config.eiros.system.default_applications.flatpak;
+
+  # Discover lives at pkgs.kdePackages.discover on most modern nixpkgs,
+  # but some channels expose it as pkgs.discover.
+  discoverPkg =
+    if pkgs ? kdePackages && pkgs.kdePackages ? discover then
+      pkgs.kdePackages.discover
+    else if pkgs ? discover then
+      pkgs.discover
+    else
+      null;
 in
 {
   options.eiros.system.default_applications.flatpak = {
@@ -14,10 +24,15 @@ in
       default = true;
       description = "Enable Flatpak support.";
     };
+
+    discover = lib.mkOption {
+      type = lib.types.bool;
+      default = true;
+      description = "Install KDE Discover as a GUI software center (recommended for non-GNOME).";
+    };
   };
 
-  config = lib.mkIf eiros_flatpak.enable {
-
+  config = lib.mkIf cfg.enable {
     services.flatpak.enable = true;
 
     systemd.services.flatpak-repo = {
@@ -26,6 +41,14 @@ in
       script = ''
         flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
       '';
+      serviceConfig.Type = "oneshot";
     };
+
+    # GUI software center support (non-GNOME)
+    services.packagekit.enable = lib.mkIf cfg.discover true;
+
+    environment.systemPackages = lib.optionals cfg.discover (
+      lib.optionals (discoverPkg != null) [ discoverPkg ]
+    );
   };
 }
