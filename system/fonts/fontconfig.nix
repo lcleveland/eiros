bf25852edb47fddeef122697bfd7cb8d444793e6
Lyle Cleveland <lyle.cleveland@proton.me>
2026-03-11 20:36:25 -0500

    test adding asian language support

--- system/fonts/fontconfig.nix
+++ system/fonts/fontconfig.nix
@@ -12,20 +12,45 @@ in
 
     default_fonts = {
       monospace = lib.mkOption {
-        default = [ "JetBrainsMono" ];
+        default = [
+          "JetBrainsMono"
+          "Noto Sans Mono CJK SC"
+          "Noto Sans Mono CJK TC"
+          "Noto Sans Mono CJK JP"
+          "Noto Sans Mono CJK KR"
+        ];
         description = "Default monospace fonts.";
         type = lib.types.listOf lib.types.str;
       };
 
       sans_serif = lib.mkOption {
-        default = [ "Inter" ];
+        default = [
+          "Inter"
+          "Noto Sans CJK SC"
+          "Noto Sans CJK TC"
+          "Noto Sans CJK JP"
+          "Noto Sans CJK KR"
+          "Noto Color Emoji"
+        ];
         description = "Default sans-serif fonts.";
         type = lib.types.listOf lib.types.str;
       };
 
       serif = lib.mkOption {
-        default = [ "Noto Serif" ];
+        default = [
+          "Noto Serif"
+          "Noto Serif CJK SC"
+          "Noto Serif CJK TC"
+          "Noto Serif CJK JP"
+          "Noto Serif CJK KR"
+        ];
         description = "Default serif fonts.";
+        type = lib.types.listOf lib.types.str;
+      };
+
+      emoji = lib.mkOption {
+        default = [ "Noto Color Emoji" ];
+        description = "Default emoji fonts.";
         type = lib.types.listOf lib.types.str;
       };
     };
@@ -38,6 +63,7 @@ in
       monospace = eiros_font_config.default_fonts.monospace;
       sansSerif = eiros_font_config.default_fonts.sans_serif;
       serif = eiros_font_config.default_fonts.serif;
+      emoji = eiros_font_config.default_fonts.emoji;
     };
   };
 }
