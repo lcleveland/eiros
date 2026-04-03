e2be7dc8c6d5a60a5a5dce39052fafbbcf195550
Lyle Cleveland <lyle.cleveland@proton.me>
2026-04-03 13:55:52 -0500

    fix loginentry

--- system/desktop_environment/mangowc.nix
+++ system/desktop_environment/mangowc.nix
@@ -9,20 +9,9 @@ in
       description = "Enable the Mango Window Composer.";
       type = lib.types.bool;
     };
-
-    add_login_entry = lib.mkOption {
-      default = false;
-      description = "Add MangoWC to the display manager login session list.";
-      type = lib.types.bool;
-    };
   };
 
   config = lib.mkIf eiros_mangowc.enable {
-    programs = {
-      mango = {
-        enable = true;
-        addLoginEntry = eiros_mangowc.add_login_entry;
-      };
-    };
+    programs.mango.enable = true;
   };
 }
