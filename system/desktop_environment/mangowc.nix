0a27e7a2e6e6d08f5d54a2ebc9d5c4674ebb8d93
Lyle Cleveland <lyle.cleveland@proton.me>
2026-04-04 13:03:49 -0500

    Revert "add MangoWC systemd session integration"
    This reverts commit 1ada35631a5086c55ca68bb4eca97855ea5f7588.

--- system/desktop_environment/mangowc.nix
+++ system/desktop_environment/mangowc.nix
@@ -9,28 +9,6 @@ in
       description = "Enable the Mango Window Composer.";
       type = lib.types.bool;
     };
-
-    systemd = {
-      enable = lib.mkOption {
-        default = true;
-        description = "Enable systemd user session integration for MangoWC. Activates graphical-session.target on startup so systemd-managed services like DMS launch correctly.";
-        type = lib.types.bool;
-      };
-
-      variables = lib.mkOption {
-        default = [
-          "DISPLAY"
-          "WAYLAND_DISPLAY"
-          "XDG_CURRENT_DESKTOP"
-          "XDG_SESSION_TYPE"
-          "NIXOS_OZONE_WL"
-          "XCURSOR_THEME"
-          "XCURSOR_SIZE"
-        ];
-        description = "Environment variables to import into the systemd user session on MangoWC startup.";
-        type = lib.types.listOf lib.types.str;
-      };
-    };
   };
 
   config = lib.mkIf eiros_mangowc.enable {
